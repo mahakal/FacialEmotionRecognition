901920e6a5725e73dd432f76b64dd10a24d8a1ff
Krishna Tripathi <mahakal@localhost.localdomain>
2016-12-08 20:59:01 +0530

    Added requirments.txt Made Changes to README.md Formalized it a bit Added Comments to cnn.py

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # Facial-Emotion-Recoginiton
+
+#Overview
 Using Extended Cohn-Kanade AU-Coded Facial Expression Database to classify basic human facial emotion expressions using ann
+
+#Installation
+
+The necessary dependencies are in the requirements.txt file so just run this before running the actual code to get them installed. May require some extra effort for OpenCV though.
+
+``
+pip3 install -r requirements.txt
+``
+
+#Usage
+
+If you want a facial emotion classifier you can just download the Cohn-Kanade database and serialize the dataset according to your need and if you can you should also use another dataset with ck.
+
+apply_haar\_cascade.py => To apply the haar cascade for face detection it will overwrite the directory with 100x100 pixels images of faces. If face couldn't be detected then print the filename to console.
+
+pickle_dataset.py => Pickles the dataset containing 100*100 images of faces
+
+cnn.py => Convolution neural Network to train the the classifier 
+
+cohn-kanade-images.txt => contains some basic info about the dataset
+
+#Credits
+- Kanade, T., Cohn, J. F., & Tian, Y. (2000). Comprehensive database for facial expression analysis. Proceedings of the Fourth IEEE International Conference on Automatic Face and Gesture Recognition (FG'00), Grenoble, France, 46-53.
+- Lucey, P., Cohn, J. F., Kanade, T., Saragih, J., Ambadar, Z., & Matthews, I. (2010). The Extended Cohn-Kanade Dataset (CK+): A complete expression dataset for action unit and emotion-specified expression. Proceedings of the Third International Workshop on CVPR for Human Communicative Behavior Analysis (CVPR4HB 2010), San Francisco, USA, 94-101.
--- cnn.py
+++ cnn.py
@@ -1,4 +1,4 @@
-from __future__ import print_function
+import pickle
 import numpy as np
 np.random.seed(1337)  # for reproducibility
 
@@ -7,8 +7,6 @@ from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.layers import Convolution2D, MaxPooling2D
 from keras.utils import np_utils
 from keras import backend as K
-
-import pickle
 
 batch_size = 30
 nb_classes = 8
@@ -27,6 +25,7 @@ kernel_size = (5, 5)
 (training_data, validation_data, test_data) = pickle.load(open('ncohn_dataset.p','rb'))
 (X_train, y_train), (X_test, y_test) = (training_data[0],training_data[1]),(test_data[0],test_data[1])
 
+#ckecks if backend is theano or tensorflow for dataset format
 if K.image_dim_ordering() == 'th':
     X_train = X_train.reshape(X_train.shape[0], 1, img_rows, img_cols)
     X_test = X_test.reshape(X_test.shape[0], 1, img_rows, img_cols)
@@ -44,8 +43,10 @@ print(X_test.shape[0], 'test samples')
 Y_train = np_utils.to_categorical(y_train, nb_classes)
 Y_test = np_utils.to_categorical(y_test, nb_classes)
 
+#A sequential model (feedforward)
 model = Sequential()
 
+#adding 2 Convolutional Layers and a maxpooling layer with activation function rectified linear unit and  Dropout for regularization
 model.add(Convolution2D(nb_filters, kernel_size[0], kernel_size[1],
                         border_mode='valid',
                         input_shape=input_shape))
@@ -55,6 +56,7 @@ model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=pool_size))
 model.add(Dropout(0.25))
 
+#A Fully Conntected Layer with relu and a output layer with softmax
 model.add(Flatten())
 model.add(Dense(128))
 model.add(Activation('relu'))
@@ -62,10 +64,12 @@ model.add(Dropout(0.5))
 model.add(Dense(nb_classes))
 model.add(Activation('softmax'))
 
+#compiling the model 
 model.compile(loss='categorical_crossentropy',
               optimizer='adadelta',
               metrics=['accuracy'])
 
+#training
 model.fit(X_train, Y_train, batch_size=batch_size, nb_epoch=nb_epoch,
           verbose=1, validation_data=(X_test, Y_test))
 score = model.evaluate(X_test, Y_test, verbose=0)
